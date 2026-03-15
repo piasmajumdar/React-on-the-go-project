d0f4a070322ff258860685f8662db46afb3e3017
piasmajumdar <piasmajumdar@gmail.com>
2026-03-15 11:07:45 +0530

    grid

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,16 +1,19 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
 
     return (
         <div>
             <h2>In the Countries: {countries.length}</h2>
-            {
-                countries.map(country=> <Country key={country.cca3.cca3} country={country}></Country>)
-            }
+            <div className='countries'>
+                {
+                    countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
     // console.log(country);
     const currencyKeys = Object.values(country.currencies.currencies);
     const currency = currencyKeys[0];
     // console.log(currency.name)
+
+    const handleVisited = ()=>{
+        console.log('btn clicked')
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Currency: {currency?.name} ({currency?.symbol})</p>
             <p>Capital: {country.capital.capital}</p>
             <p>Region: {country.region.region}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ?"Big Country": "Small Country"}</p>
+            <button onClick={handleVisited}>Not visited</button>
         </div>
     );  
 };
