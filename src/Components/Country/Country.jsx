2366a8ceb87d5dccece17693178eb0a5a7b3ca76
piasmajumdar <piasmajumdar@gmail.com>
2026-03-15 11:34:15 +0530

    toggle visited with css

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
     // console.log(country);
     const currencyKeys = Object.values(country.currencies.currencies);
     const currency = currencyKeys[0];
     // console.log(currency.name)
 
-    const handleVisited = ()=>{
-        console.log('btn clicked')
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+
+        // Method 1: 
+        // if(visited){
+        //     setVisited(false);
+        // }
+        // else {
+        //     setVisited(true);
+        // }
+
+        // Method 2:
+        // setVisited(visited ? false : true)
+
+        // Method 3:
+        setVisited(!visited)
     }
     return (
-        <div className='country'>
+        //<div className={`country ${visited ? 'country-visited' " 'country-not-visited'"}`}
+        <div className={`country ${visited && 'visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Currency: {currency?.name} ({currency?.symbol})</p>
             <p>Capital: {country.capital.capital}</p>
             <p>Region: {country.region.region}</p>
-            <p>Area: {country.area.area} {country.area.area > 300000 ?"Big Country": "Small Country"}</p>
-            <button onClick={handleVisited}>Not visited</button>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
-    );  
+    );
 };
 
 export default Country;
