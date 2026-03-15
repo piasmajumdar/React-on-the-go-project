2f9e842932f41e28afc8b12721454c95d47738d0
piasmajumdar <piasmajumdar@gmail.com>
2026-03-15 08:39:31 +0530

    country added

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,12 +1,16 @@
 import React, { use } from 'react';
+import Country from '../Country/Country';
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
-    console.log(countries);
+
     return (
         <div>
             <h2>In the Countries: {countries.length}</h2>
+            {
+                countries.map(country=> <Country key={country.cca3.cca3} country={country}></Country>)
+            }
         </div>
     );
 };
