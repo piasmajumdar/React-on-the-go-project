daa99ba7cddf0e2c4ae0cdc700e1c8da24ad9059
piasmajumdar <piasmajumdar@gmail.com>
2026-03-15 10:16:54 +0530

    showed currency, capital, region

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    // console.log(country)
+    // console.log(country.region.region);
+    const currencyKeys = Object.values(country.currencies.currencies);
+    const currency = currencyKeys[0];
+    // console.log(currency.name)
     return (
         <div>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Currency: {currency?.name} ({currency?.symbol})</p>
+            <p>Capital: {country.capital.capital}</p>
+            <p>Region: {country.region.region}</p>
         </div>
-    );
+    );  
 };
 
 export default Country;
